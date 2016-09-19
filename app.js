02202b4c3d38d79fef4e126bf79986d848259cfc
Matt Schwellensattl <mattschwell@gmail.com>
2016-09-19 00:19:16 -0500

    finishing assignment

--- app.js
+++ app.js
@@ -2,11 +2,28 @@
 'use strict';
 
 angular.module('LunchCheck', [])
-.controller('LunchCheckController', MsgController);
+.controller('LunchCheckController', LunchCheckController);
 
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
-  
+  $scope.items = "";
+  $scope.displayMessageText = "";
+
+  $scope.displayMessage = function () {
+    var displayMessageValue = getMessageText($scope.items);
+    $scope.displayMessageText = displayMessageValue;
+  };
+
+    function getMessageText(items) {
+      var stringSplit = items.split(',');
+      var returString = "";
+      if(stringSplit.length <= 3) {
+        returString = "Enjoy!";
+      } else {
+        returString = "Too much!";
+      }
+      return returString;
+    }
 }
 
 })();
